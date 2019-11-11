54646a0301fa2b9c6eabcfca250abc5ff4be6b78
Urs Minder <urs.minder@centeractive.com>
2019-11-11 15:35:42 +0100

    Azure DevOps development

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -8,8 +8,6 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-junit-reporter')
@@ -50,25 +48,6 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    customLaunchers: {
-      ChromeDebug: {
-        base: 'Chrome',
-        flags: [
-          '--remote-debugging-port=9222'
-        ],
-        debug: true
-      },
-      ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--headless',
-          '--no-sandbox',
-          '--disable-gpu',
-          '--remote-debugging-port=9222'
-        ],
-        debug: false
-      }
-    },
     browsers: ['ChromeHeadless'],
     singleRun: false
   });
