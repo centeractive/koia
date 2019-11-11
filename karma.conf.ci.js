36cd90dae12be8e9656490f01d3eb078eea61977
Urs Minder <urs.minder@centeractive.com>
2019-11-11 16:03:28 +0100

    Azure DevOps development

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -8,6 +8,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
