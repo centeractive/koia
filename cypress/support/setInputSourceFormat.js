6139eb40d430245a56b65a2f79689dc460f1a1fc
vonGuntenMartin <107695499+vonGuntenMartin@users.noreply.github.com>
2022-07-22 15:23:59 +0200

    check removed

--- cypress/support/setInputSourceFormat.js
+++ cypress/support/setInputSourceFormat.js
@@ -1,19 +1,4 @@
 export function setInputSourceFormat(inputSourceFormat) {
-  if (
-    Object.values(cy.get(".input_source_format").includes(
-      inputSourceFormat
-    )
-  )
-  } else {
-    var flattened = flatval(cy.get(".input_source_format").eq(0));
-    cy.log("flat : " + flattened); // ALL VALUES IN FLAT ARRAY
-    cy.log("falt : " + flattened.includes("Pizza")); // true
-
-    cy.get(".input_source_format")
-      .eq(0)
-      .clear()
-      .type(inputSourceFormat)
-      .click(); //SourceDateFormat
-    cy.get(".select_data_type").click;
-  }
+  cy.get(".input_source_format").eq(0).clear().type(inputSourceFormat).click(); //SourceDateFormat
+  cy.get(".select_data_type").click;
 }
