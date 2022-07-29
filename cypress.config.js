960113b2ae23ed37c656a43d4a52c907bf7e54e7
vonGuntenMartin <107695499+vonGuntenMartin@users.noreply.github.com>
2022-07-29 13:02:24 +0200

    mochawesome added

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "d7bpee",
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "cypress-mochawesome-reporter, mocha-junit-reporter",
+    mochaJunitReporterReporterOptions: {
+      mochaFile: "cypress/reports/junit/results-[hash].xml",
+    },
+    cypressMochawesomeReporterReporterOptions: {
+      charts: true,
+      reportPageTitle: "Koia.io date test",
+    },
+  },
+  video: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
+  },
+});
